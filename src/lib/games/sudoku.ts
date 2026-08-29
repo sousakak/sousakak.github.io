cb2d34640a515fd9183941f8b5f2163edd17df94
sousakak <sousakak7@gmail.com>
2026-08-29 21:36:58 +0900

    feat: enhance fillHiddenSingles function to improve candidate elimination logic

--- src/lib/games/sudoku.ts
+++ src/lib/games/sudoku.ts
@@ -1,11 +1,18 @@
-import { shuffleArray } from '../utils/random'
+import { shuffleArray } from '../utils/random';
 
-interface SolveResult {
+export interface SolveResult {
     isSuccess: boolean,
     boardResult: number[][]
-}
+};
 
-type Board = number[][]
+export type Board = number[][];
+
+function* getCol<T>(tbl: T[][]): Generator<[T[], number], void, unknown> {
+    for (let i = 0; i < tbl[0].length; i++) {
+        const columnData = tbl.map(r => r[i]);
+        yield [columnData, i];
+    }
+}
 
 export function solve( board: Board ): SolveResult {
     let boardCopy = structuredClone( board );
@@ -65,7 +72,7 @@ export function fillNakedSingles( board: Board ): boolean {
     for ( const [ i, boardRow ] of board.entries() ) {
         for ( const [ j, boardCell ] of boardRow.entries() ) {
             if ( boardCell === 0 ) {
-                let candidates = getCandidates( board, i, j );
+                const candidates = getCandidates( board, i, j );
                 if ( candidates.length === 1 ) {
                     board[i][j] = candidates[0];
                     updated = true;
@@ -78,5 +85,69 @@ export function fillNakedSingles( board: Board ): boolean {
 
 export function fillHiddenSingles( board: Board ): boolean {
     let updated = false;
+
+    for ( const [ i, boardRow ] of board.entries() ) {
+        let possibleCols: Record<number, number[]> = {};
+        for ( const [ j, boardCell ] of boardRow.entries() ) {
+            if ( boardCell === 0 ) {
+                const candidates = getCandidates( board, i, j );
+                candidates.forEach( num => {
+                    possibleCols[num] = possibleCols[num]
+                        ? [...possibleCols[num], j]
+                        : [j];
+                } );
+            }
+        }
+        for ( const [ num, cols ] of Object.entries( possibleCols ) ) {
+            if ( cols.length === 1 ) {
+                board[i][possibleCols[+num][0]] = +num;
+                updated = true;
+            }
+        }
+    }
+
+    for ( const [ boardCol, i ] of getCol( board ) ) {
+        let possibleRows: Record<number, number[]> = {};
+        for ( const [ j, boardCell ] of boardCol.entries() ) {
+            if ( boardCell === 0 ) {
+                const candidates = getCandidates( board, i, j );
+                candidates.forEach( num => {
+                    possibleRows[num] = possibleRows[num]
+                        ? [...possibleRows[num], j]
+                        : [j];
+                } );
+            }
+        }
+        for ( const [ num, rows ] of Object.entries( possibleRows ) ) {
+            if ( rows.length === 1 ) {
+                board[i][possibleRows[+num][0]] = +num;
+                updated = true;
+            }
+        }
+    }
+
+    for ( let chunk = 0; chunk < 9; chunk++ ) {
+        const [ boxC, boxR ] = [Math.floor(chunk % 3) * 3, Math.floor(chunk / 3) * 3];
+        let possibleCells: Record<number, number[][]> = {};
+        for ( let i = boxR; i < boxR + 3; i++ ) {
+            for ( let j = boxC; j < boxC + 3; j++ ) {
+                if ( board[i][j] === 0 ) {
+                    const candidates = getCandidates( board, i, j );
+                    candidates.forEach( num => {
+                        possibleCells[num] = possibleCells[num]
+                            ? [...possibleCells[num], [i, j]]
+                            : [[i, j]];
+                    } );
+                }
+            }
+        }
+        for ( const [ num, cells ] of Object.entries( possibleCells ) ) {
+            if ( cells.length === 1 ) {
+                board[cells[0][0]][cells[0][1]] = +num;
+                updated = true;
+            }
+        }
+    }
+
     return updated;
 }
